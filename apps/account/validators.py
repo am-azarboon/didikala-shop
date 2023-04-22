ff737d9a5e8dbbd1bb3c2e86e2d67a18210c55b2
AmirMahdi Azarboon <amirazarboon@gmail.com>
2023-04-22 13:09:53 +0330

    email and mobile format validators added

--- apps/account/validators.py
+++ apps/account/validators.py
@@ -1,5 +1,8 @@
 from django.utils.translation import gettext_lazy as _
 from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
+import random
+import re
 
 
 # Number Only validator
@@ -15,3 +18,20 @@ def positive_number_valid(value):
 
         if flag is False:
             raise ValidationError(_('Invalid melli code'), code='invalid_melli_code')
+
+
+# Check mobile number format(Iran)
+def mobile_format_check(string):
+    mobile_regex = "^09(1[0-9]|3[1-9]|2[1-9])-?[0-9]{3}-?[0-9]{4}$"
+    if re.search(mobile_regex, string):
+        return True
+    return False
+
+
+# Check email format
+def email_format_check(string):
+    try:
+        validate_email(string)
+        return True
+    except ValidationError:
+        return False
