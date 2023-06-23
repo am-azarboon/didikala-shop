30840ff729a77cf771500baec62fc2ec3f1d9f4a
AmirMahdi Azarboon <amirazarboon@gmail.com>
2023-06-23 23:58:25 +0330

    Clean some codes

--- didikala/urls.py
+++ didikala/urls.py
@@ -7,12 +7,12 @@ from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/', include('apps.account.urls', namespace='account')),
-    path('address/', include('apps.address.urls', namespace='address')),
-    path('product/', include('apps.product.urls', namespace='product')),
-    path('cart/', include('apps.cart.urls', namespace='cart')),
-    path('order/', include('apps.order.urls', namespace='order')),
-    path('', include('apps.main.urls', namespace='main')),
+    path('account/', include('apps.account.urls', namespace="account")),
+    path('address/', include('apps.address.urls', namespace="address")),
+    path('product/', include('apps.product.urls', namespace="product")),
+    path('cart/', include('apps.cart.urls', namespace="cart")),
+    path('order/', include('apps.order.urls', namespace="order")),
+    path('', include('apps.main.urls', namespace="main")),
     path('bankgateways/', az_bank_gateways_urls()),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
